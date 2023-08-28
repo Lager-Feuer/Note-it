d17437776a383aae3479f5f84e4a5cf90028f5a0
AHL <Ahlers@boehme-weihs.de>
2023-08-28 14:10:21 +0200

    Big bang

---
+++ Note_it/Model/Notebook.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Note_it.Model
+{
+    class Notebook : ViewModelBase
+    {
+        public Notebook()
+        {
+            this.Notizen = new ObservableCollection<Note>();
+        }
+
+        private string _title;
+
+        public ObservableCollection<Note> Notizen { get; set; }
+        public string Title { get { return _title; } set { _title = value; NotifyPropertyChanged(); } }
+    }
+}
---
+++ Note_it/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Note_it
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string porpertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(porpertyName));
+            }
+        }
+    }
+}
